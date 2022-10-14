4c508ea173ab5213e9a915c8252d17de37a90696
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-10-14 15:50:23 -0300

    feat: add about me and personal info jsx

--- Client/src/presentation/pages/home/home.tsx
+++ Client/src/presentation/pages/home/home.tsx
@@ -1,9 +1,36 @@
 import Layout from '@/presentation/layout/layout';
 import React from 'react';
-import { Container } from './home-styles';
+import { AboutMe, Container, PersonalInfo, Text, Title } from './home-styles';
 
 const Home = () => {
-  return <Layout>home</Layout>;
+  return (
+    <Layout>
+      <Container>
+        <AboutMe>
+          <Title>About Me</Title>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas
+            vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum
+            quisquam eius sed odit fugiat iusto fuga praesentium
+          </Text>
+        </AboutMe>
+        <PersonalInfo>
+          <div>
+            <Title size={1.2}>Location</Title>
+            <Text>São Paulo, Brasil</Text>
+          </div>
+          <div>
+            <Title size={1.2}>Job</Title>
+            <Text>Developer</Text>
+          </div>
+          <div>
+            <Title size={1.2}>E-mail</Title>
+            <Text>fbarreto.dev@gmail.com</Text>
+          </div>
+        </PersonalInfo>
+      </Container>
+    </Layout>
+  );
 };
 
 export default Home;
