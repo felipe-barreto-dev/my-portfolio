e86083ebd4a186eefdf509c9a354a570dc988573
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-10-14 16:23:52 -0300

    feat: add projects

--- client/src/presentation/pages/home/home-styles.ts
+++ client/src/presentation/pages/home/home-styles.ts
@@ -22,6 +22,7 @@ export const AboutMe = styled.section`
 
 export const Title = styled.span`
   font-weight: 700;
+  margin-left: ${(props) => props.left + 'px'};
   font-size: ${(props) => (props.size ? props.size + 'rem' : '1.5rem')};
 `;
 
@@ -30,11 +31,58 @@ export const Text = styled.p`
   font-size: 1rem;
 `;
 
-export const PersonalInfo = styled.div`
+export const PersonalInfo = styled.section`
   margin-top: 50px;
   background-color: #23395b;
   border-radius: 12px;
   display: flex;
   justify-content: space-between;
   padding: 20px 70px;
+`;
+
+export const SeparatorLine = styled.hr`
+  margin: 50px 20px;
+`;
+
+export const ProjectsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Projects = styled.div`
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: 30% 30% 30%;
+  gap: 4.5%;
+`;
+
+export const Project = styled.div`
+  background-color: #23395b;
+  display: grid;
+  border-radius: 12px;
+  padding: 20px;
+  gap: 10px;
+
+  div {
+    display: flex;
+    justify-content: end;
+  }
+`;
+
+export const ProjectInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
