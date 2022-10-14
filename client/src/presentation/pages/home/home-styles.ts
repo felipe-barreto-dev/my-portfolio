68e397d73f03bbb8a576464c16dacc2daca598fa
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-10-14 15:49:21 -0300

    style: add personal info container

--- client/src/presentation/pages/home/home-styles.ts
+++ client/src/presentation/pages/home/home-styles.ts
@@ -22,10 +22,19 @@ export const AboutMe = styled.section`
 
 export const Title = styled.span`
   font-weight: 700;
-  font-size: 1.5rem;
+  font-size: ${(props) => (props.size ? props.size + 'rem' : '1.5rem')};
 `;
 
 export const Text = styled.p`
   font-weight: 500;
   font-size: 1rem;
+`;
+
+export const PersonalInfo = styled.div`
+  margin-top: 50px;
+  background-color: #23395b;
+  border-radius: 12px;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 70px;
 `;
